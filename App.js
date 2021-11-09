a92af74543555ee360103f86e6043143b44ef3c7
Santiago Hewitt Ramirez <hewitt@une.net.co>
2021-11-08 20:58:03 -0500

    cambio uri de mongo

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ const port = process.env.PORT ||3001;
 
 //Configurar la conexion con mongo atlas
 const mongoose = require('mongoose');
-const URI = 'mongodb+srv://AplicacionWebVentas:ROjYw8UuU3nqOGiw@cluster0.l4uir.mongodb.net/myFirstmitg3?retryWrites=true&w=majority'
+const URI = `mongodb+srv://AplicacionWebVentas:ROjYw8UuU3nqOGiw@cluster0.l4uir.mongodb.net/mitg3?retryWrites=true&w=majority`
 const option ={useNewUrlParser: true, useUnifiedTopology: true};
 mongoose.connect(URI, option)
 .then(()=> console.log("Base de datos Conectada correctamente"))
